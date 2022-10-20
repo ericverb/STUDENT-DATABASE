7ab4e03a90a783219ac2bbb344bcfc60b5d5b74d
Verbeeren, Eric <EricVerbeeren@RocketCentral.com>
2022-10-20 17:05:43 -0400

    Back to Orginal Submission

--- STUDENT DATABASE/Program.cs
+++ STUDENT DATABASE/Program.cs
@@ -10,53 +10,23 @@ bool goAgain = true;
 while (goAgain)
 {
     Console.WriteLine(
-        $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}! or 0 to see all students! {Environment.NewLine}");
+        $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}! {Environment.NewLine}");
     int studentNumber;
     bool isValid = int.TryParse(Console.ReadLine(), out studentNumber);
-    bool validCategory = false;
-    string userCategory = "";
-    if (studentNumber == 0)
-    {
-        foreach (var name in names)
-        {
-            studentNumber++;
-            Console.WriteLine($"{studentNumber} - {name}");
-        }
 
-        Console.WriteLine(
-            $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}!{Environment.NewLine}");
-
-        isValid = int.TryParse(Console.ReadLine(), out studentNumber);
-        Console.WriteLine(
-            $"Student {studentNumber} is {names.ElementAt(studentNumber - 1)}. What would you like to know? Enter hometown or favorite food{Environment.NewLine}");
-        userCategory = Console.ReadLine()!.ToLower().Trim().Replace(" ", "");
-        validCategory = StudentCategoryValid(userCategory);
-    }
-    else if (studentNumber >= 1 || studentNumber <= names.Length)
+    while (studentNumber < 1 || studentNumber > names.Length)
     {
         Console.WriteLine(
-            $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}!{Environment.NewLine}");
-
-        isValid = int.TryParse(Console.ReadLine(), out studentNumber);
+            $"Your Number {studentNumber} is not within the correct range of numbers{Environment.NewLine}");
         Console.WriteLine(
-            $"Student {studentNumber} is {names.ElementAt(studentNumber - 1)}. What would you like to know? Enter hometown or favorite food{Environment.NewLine}");
-        userCategory = Console.ReadLine()!.ToLower().Trim().Replace(" ", "");
-        validCategory = StudentCategoryValid(userCategory);
-    }
-    else
-    {
-        while (studentNumber < 1 || studentNumber > names.Length)
-        {
-            Console.WriteLine(
-                $"Your Number {studentNumber} is not within the correct range of numbers{Environment.NewLine}");
-            Console.WriteLine(
-                $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}! {Environment.NewLine}");
-            bool isValidAgain = int.TryParse(Console.ReadLine(), out studentNumber);
-        }
+            $"Welcome! Which student would you like to learn more about? Enter a number 1-{names.Length}! {Environment.NewLine}");
+        bool isValidAgain = int.TryParse(Console.ReadLine(), out studentNumber);
     }
 
-
-    ;
+    Console.WriteLine(
+        $"Student {studentNumber} is {names.ElementAt(studentNumber - 1)}. What would you like to know? Enter hometown or favorite food{Environment.NewLine}");
+    string userCategory = Console.ReadLine()!.ToLower().Trim().Replace(" ", "");
+    bool validCategory = StudentCategoryValid(userCategory);
 
     while (!validCategory)
     {
@@ -82,10 +52,12 @@ while (goAgain)
         string tryAgain = Console.ReadLine()!.ToLower().Trim();
         goAgain = GetStudent(tryAgain);
     }
+
 }
 
 Console.WriteLine("Thanks!");
 Environment.Exit(0);
+
 
 
 bool StudentCategoryValid(string userSelection)
